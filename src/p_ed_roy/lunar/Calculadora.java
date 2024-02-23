7007ff2551903e6d9320a77c6067ed76f60c37e0
roy <lunarmorer@gmail.com>
2024-02-23 16:55:30 +0100

    Proyecto CalculadoraJava

---
+++ src/p_ed_roy/lunar/Calculadora.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p_ed_roy.lunar;
+
+/**
+ *
+ * @author Linkm
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+        System.out.println(sumar(5, 5)  );
+    }
+    
+    static int sumar(int num1, int num2){
+        int resultado = num1 + num2;
+        return resultado;
+    }
+}
